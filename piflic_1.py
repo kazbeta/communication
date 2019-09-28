5010f82627d910ac03366416bc58d7b5cb5e0559
Kaz <45856707+kazbeta@users.noreply.github.com>
2019-09-27 17:21:01 -0700

    Update piflic_1.py

--- piflic_1.py
+++ piflic_1.py
@@ -14,7 +14,7 @@ hold = "hold"
 while True:
     try:
 	# SUBSTITUTE INSERT HERE WITH YOUR PUSHBULLET TOKEN
-	push = os.popen("curl --header 'Access-Token:o.PQJFg97pbDuyKvzOhsbUoMuzLfnSM7EE' --data-urlencode active='true' --data-urlencode modified_after='1.4e+09' --get https://api.pushbullet.com/v2/pushes")
+	push = os.popen("curl --header 'Access-Token: o.qwRX0BTs1O6BVqUoRqDRsqKFWY35jRDy' --data-urlencode active='true' --data-urlencode modified_after='1.4e+09' --get https://api.pushbullet.com/v2/pushes")
 	line = push.readline()
 	print line
 	print push
@@ -23,17 +23,17 @@ while True:
        		time.sleep(3)
 		GPIO.output(18,GPIO.LOW)
 		# SUBSTITUTE INSERT HERE WITH YOUR PUSHBULLET TOKEN
-		os.system("curl --header 'Access-Token:o.PQJFg97pbDuyKvzOhsbUoMuzLfnSM7EE' --request DELETE https://api.pushbullet.com/v2/pushes")
+		os.system("curl --header 'Access-Token: o.qwRX0BTs1O6BVqUoRqDRsqKFWY35jRDy' --request DELETE https://api.pushbullet.com/v2/pushes")
 	if dclick in line:
 		GPIO.output(18,GPIO.HIGH)
                 time.sleep(1)
 		# SUBSTITUTE INSERT HERE WITH YOUR PUSHBULLET TOKEN
-                os.system("curl --header 'Access-Token:o.PQJFg97pbDuyKvzOhsbUoMuzLfnSM7EE' --request DELETE https://api.pushbullet.com/v2/pushes")
+                os.system("curl --header 'Access-Token: o.qwRX0BTs1O6BVqUoRqDRsqKFWY35jRDy' --request DELETE https://api.pushbullet.com/v2/pushes")
 	if hold in line:
                 time.sleep(0.2)
 		GPIO.output(18,GPIO.LOW)
 		# SUBSTITUTE INSERT HERE WITH YOUR PUSHBULLET TOKEN
-                os.system("curl --header 'Access-Token:o.PQJFg97pbDuyKvzOhsbUoMuzLfnSM7EE' --request DELETE https://api.pushbullet.com/v2/pushes")
+                os.system("curl --header 'Access-Token: o.qwRX0BTs1O6BVqUoRqDRsqKFWY35jRDy' --request DELETE https://api.pushbullet.com/v2/pushes")
 
     except KeyboardInterrupt:
 	# Turn LED off before stopping
